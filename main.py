d71d0a66a17babc938cdfd93ccda6b966afc6a4a
shimae naga <isagi.kazui@gmail.com>
2024-08-20 11:05:25 +0900

    0820

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Flask import Flask, render_template # type: ignore
+from flask import Flask, render_template, request, jsonify
 import utils.spotify_client as spotify_client
 
 app = Flask(__name__)
@@ -6,6 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/create_playlist', methods=['POST'])
+def create_playlist():
+    data = request.json
+    tempo = data.get('tempo')
+    
+    if tempo:
+        # Spotify APIを使って曲を検索し、オーディオフィーチャーを取得
+        track_ids = spotify_client.search_tracks_by_tempo(tempo)
+        tracks_info = spotify_client.get_tracks_info(track_ids)
+
+        return jsonify({'tracks': tracks_info})
+    return jsonify({'error': 'No tempo provided'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
--- utils/spotify_client.py
+++ utils/spotify_client.py
@@ -9,5 +9,22 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope="user-library-read"
 ))
 
-def get_audio_features(track_ids):
-    return sp.audio_features(track_ids)
+def search_tracks_by_tempo(tempo, limit=10):
+    # ターゲットテンポの範囲を設定
+    min_tempo = max(0, tempo - 5)
+    max_tempo = tempo + 5
+
+    # Spotify APIで楽曲を検索
+    results = sp.search(q=f'tempo:{min_tempo}-{max_tempo}', type='track', limit=limit)
+    track_ids = [track['id'] for track in results['tracks']['items']]
+    return track_ids
+
+def get_tracks_info(track_ids):
+    tracks = sp.tracks(track_ids)['tracks']
+    tracks_info = []
+    for track in tracks:
+        tracks_info.append({
+            'name': track['name'],
+            'artist': track['artists'][0]['name']
+        })
+    return tracks_info
